6ab248456cf2071c406872f608a9e30267d69055
wb-rahuljaim595360 <wb-rahuljaim595360@antfin.com>
2020-04-23 14:07:18 +0530

    creating tarne app for local deliverables

---
+++ src/main/java/com/jaimanandjain/tarne/TarneApplication.java
@@ -0,0 +1,13 @@
+package com.jaimanandjain.tarne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaimanandjain/tarne/TarneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaimanandjain.tarne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
