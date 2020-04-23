3cfcee8fee316ed3a616d359bfb4fe9600dab79d
wb-rahuljaim595360 <wb-rahuljaim595360@antfin.com>
2020-04-23 18:55:14 +0530

    Setting up the files

--- src/main/java/com/jaimanandjain/tarne/TarneApplication.java
+++ src/main/java/com/jaimanandjain/tarne/TarneApplication.java
@@ -2,12 +2,21 @@ package com.jaimanandjain.tarne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TarneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TarneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TarneApplication.class, args);
+    }
+
+    @RequestMapping(value = "/")
+    public String sayHello(){
+        return "Rahul Jaiman";
+    }
+
 
 }
