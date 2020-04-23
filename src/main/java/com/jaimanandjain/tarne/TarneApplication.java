9893a7893c7a90971a9e3f290c47a9138fc250a2
Rahul Jaiman <revashankar26@gmail.com>
2020-04-23 20:38:11 +0530

    created war file

--- src/main/java/com/jaimanandjain/tarne/TarneApplication.java
+++ src/main/java/com/jaimanandjain/tarne/TarneApplication.java
@@ -2,20 +2,26 @@ package com.jaimanandjain.tarne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class TarneApplication {
+public class TarneApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        return springApplicationBuilder.sources(TarneApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(TarneApplication.class, args);
     }
 
     @RequestMapping(value = "/")
     public String sayHello(){
-        return "Let's build some epic things !!";
+        return "Let's build some epic things !! Tomcat Started";
     }
 
 
